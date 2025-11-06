50b9286f30b156312f91bdc0d78c5f273d1647a0
Helô <129074504+hel00322@users.noreply.github.com>
2025-11-06 09:46:01 -0300

    Update main.py
    criando o main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from software_treino import *
 
 def menu():
-    print("MENU SISTEMA GERADOR DE TREINOS")
+    print("\nMENU SISTEMA GERADOR DE TREINOS")
     print("================*================")
     print("1) Cadastrar aluno")
     print("2) Alunos cadastrados")
@@ -14,7 +14,7 @@ def main():
 
     while True:
         menu()
-        opcao = input("Selecione uma opção: ")
+        opcao = input("\nSelecione uma opção: ")
 
         if opcao == "1":
             print("\nCadastrar novo auno:")
@@ -24,4 +24,57 @@ def main():
             peso = float(input("Peso (kg): "))
             altura = float(input("Altura (m): "))
 
-            
+            aluno = Aluno(nome, data, cpf, peso, altura)
+            alunos.append(aluno)
+
+            print(f"\nAluno(a) {nome} cadastrado(a) com sucesso!")
+
+        elif opcao == "2":
+            if not alunos:
+                print("\n Nenhum aluno cadastrado")
+            else:
+                print("\n==== Alunos Cadastrados ===")
+                for i, aluno in enumerate(alunos):
+                    print(f"\n[{i + 1}]")
+                    print(aluno)
+
+        elif opcao == "3":
+            if not alunos:
+                print("\nNenhum aluno cadastrado, cadestre um aluno para consultar os treinos")
+                continue
+
+            for i, aluno in enumerate(alunos):
+                print(f"{i + 1}. {aluno._Pessoa__nome}")
+            escolha = int(input("Escolha o número do aluno: ")) - 1
+
+            if 0 <= escolha < len(alunos):
+                print(f"\nTreinos de {alunos[escolha]._Pessoa__nome}:")
+                alunos[escolha].visualizar_Treino()
+            else:
+                print("Aluno inválido!")
+
+        elif opcao == "4":
+            if not alunos:
+                print("\nNenhum aluno cadastrado")
+                continue
+
+            for i, aluno in enumerate(alunos):
+                print(f"{i + 1}. {aluno._Pessoa__nome}")
+            escolha = int(input("Escolha o número do aluno: ")) - 1
+
+            if 0 <= escolha < len(alunos):
+                treino = input("Digite o nome do treino para adicionar: ")
+                alunos[escolha].treinos.append(treino)
+                print(f"Treino '{treino}' adicionado para {alunos[escolha]._Pessoa__nome}")
+            else:
+                print("Aluno inválido!")
+
+        elif opcao == "5":
+            print("\nSaindo do sistema...")
+            break
+
+        else:
+            print("Opção inválida. Tente novamente.")
+
+if __name__ == "__main__":
+    main()
